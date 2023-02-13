bf7048a1827f239c2f8c3ac2f613b3dd0bbd4797
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-02-13 17:46:28 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,32 @@ $(function () {
     });
     $('.header .gnb').on('mouseleave', function () {
         $('.header').removeClass('on');
+        if ($(window).width() < 769) {
+            $('.header').addClass('on');
+        }
+    });
+
+    $('.header button').on('click', function () {
+        var sct = $(window).scrollTop();
+        $('.header .gnb').toggleClass('on');
+        $(this).toggleClass('on');
+        if ($('.header button').hasClass('on')) {
+            $('.header').addClass('bg')
+        }
+    });
+
+    $('.header .gnb>ul>li>a').on('click', function (e) {
+        if ($(window).width() < 769) {
+            e.preventDefault();
+            $(this).parent().siblings().find('a').next().removeClass('on');
+            $(this).next().toggleClass('on');
+        }
+    });
+
+    $('.header button').on('scroll wheel touchmove', function () {
+        if ($(this).hasClass('on')) {
+            return false;
+        }
     });
 
     $(window).on('scroll', function () {
@@ -47,6 +73,16 @@ $(function () {
     $('.news_slide').slick({
         arrows: false,
         slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+        ],
     });
 
     $('.news_prev').on('click', function () {
@@ -59,6 +95,16 @@ $(function () {
     $('.sns_slide').slick({
         arrows: false,
         slidesToShow: 4,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+        ],
     });
 
     $('.sns_prev').on('click', function () {
@@ -77,4 +123,10 @@ $(function () {
     $('.toTop').on('click', function () {
         $('html, body').stop().animate({ scrollTop: 0 }, 1000);
     });
+    // main script///////////////
+
+    (function () {
+        var num = 2;
+        $('.aside_menu ul>li').eq(num - 1).addClass('on');
+    })
 })
